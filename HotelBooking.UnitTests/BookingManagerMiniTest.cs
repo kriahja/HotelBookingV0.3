364fa9a2f3707bcfe1374e9226d26ac5c18cc722
Kristjan Aron Hjartarson <kriahja@gmail.com>
2017-04-27 15:55:36 +0200

    Compulsory ready-ish

--- HotelBooking.UnitTests/BookingManagerMiniTest.cs
+++ HotelBooking.UnitTests/BookingManagerMiniTest.cs
@@ -1,47 +1,17 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using HotelBooking.BLL;
+﻿using HotelBooking.BLL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Diagnostics;
 
-
-namespace HotelBooking.BLL.Tests
+namespace HotelBooking.UnitTests
 {
     [TestClass()]
-    public class BookingManagerTests
+    public class BookingManagerMiniTest
     {
 
-        [TestMethod()]
-        public void FindAvailableRoom_StartDateNotInTheFuture_ThrowsArgumentException()
-        {
-            BookingManager manager = CreateBookingManager();
-            DateTime date = DateTime.Today;
-
-            try
-            {
-                manager.FindAvailableRoom(date, date);
-            }
-            catch (ArgumentException)
-            {
-
-            }
-            catch (Exception)
-            {
-                Assert.Fail();
-            }
-        }
-
-        [TestMethod()]
-        public void FindAvailableRoom_RoomAvailable_RoomIdNotMinusOne()
-        {
-            BookingManager manager = CreateBookingManager();
-            DateTime date = DateTime.Today.AddDays(1);
-            int roomId = manager.FindAvailableRoom(date, date);
-            Assert.AreNotEqual(-1, roomId);
-        }
 
         [TestMethod()]
         public void FindavAvailableRoom_WithSpecificRoomId()
@@ -62,23 +32,21 @@ namespace HotelBooking.BLL.Tests
         public void GetFullyOccupiedDates()
         {
             BookingManager manager = CreateBookingManager();
-            List<DateTime> knownDates= new List<DateTime>();
+            List<DateTime> knownDates = new List<DateTime>();
             for (int i = 0; i <= 14; ++i)
             {
                 knownDates.Add(new DateTime(2017, 4, 20 + i));
             }
-            
+
             List<DateTime> dates = manager.GetFullyOccupiedDates(DateTime.Today, DateTime.Today.AddYears(1));
-            
+
             Assert.IsTrue(listcomparer(knownDates, dates));
         }
 
-
-
         public bool comparer(DateTime a, DateTime b)
         {
-           
-            if (DateTime.Equals(a,b))
+
+            if (DateTime.Equals(a, b))
             {
                 return true;
             }
